d5f5e701662bf2590caf2342099f9c876d9ce809
MarieAngeLeslie <marieangeleslie0305@gmail.com>
2023-11-10 11:35:12 +0000

    feat:delete_Specific_Elment_At_Specific_Index

--- fullstack.io/linkedList/LinkedList.js
+++ fullstack.io/linkedList/LinkedList.js
@@ -77,15 +77,53 @@ class LinkedList {
             console.log(`Your value at index ${index} is : ${currentElt.value}`);
         }
     }
-    //CLear List
 
     //Delete specific element with it's index
+    deleteElmentAtIndex(index) {
+
+        /**v
+         * Verifier si le linked list est vide
+         * si pas vide, et que : 
+         * 1- index == 0  sauvegarder l'ancienne valeur et lié le head au next
+         * 2- si non, vérifier si la taille de la chaine est supérieure ou égale à l'index+1
+         *  si oui, lié le précédent au prochain là bas
+         */
+        if (index < 0 || index >= this.checkLinkedListSize()) {
+            console.log("l'élément que vous recherchez est impossible à trouver");
+            return null;
+        }
+
+        if (this.head) {
+            let deletedNode = null;
+            if (index === 0) {
+                deletedNode = this.head;
+                this.head = this.head.next;
+            } else {
+                let currentElt = this.head;
+                let previousElt = null;
+                let count = 0;
+
+                //find previous Elt
+                while (currentElt && count != index - 1) {
+                    currentElt = currentElt.next;
+                    count++;
+                }
+                deletedNode = currentElt.next;
+                previousElt = currentElt;
+                previousElt.next = currentElt.next.next;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    //CLear List
 
     //deleteTail
 
     //deleteHead
 
-    //
+
 
     printDatasList() {
         let current = this.head;
@@ -98,13 +136,17 @@ class LinkedList {
 }
 
 const ourTest = new LinkedList();
-// ourTest.append(60);
-// ourTest.append(170);
-// ourTest.append(36);
-// ourTest.append(129);
+ourTest.append(60);
+ourTest.append(170);
+ourTest.append(36);
+ourTest.append(129);
 // ourTest.prepend(39);
 // console.log(ourTest.checkLinkedListSize());
 // ourTest.insertAtSpecificIndex(3, 504);
 
-// ourTest.printDatasList();
-ourTest.getValueAtIndex(2);
+// ourTest.getValueAtIndex(2);
+ourTest.printDatasList();
+console.log(ourTest.checkLinkedListSize());
+ourTest.deleteElmentAtIndex(4);
+console.log("Après suppression");
+ourTest.printDatasList();
